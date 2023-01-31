9dd9f3770447f2c2660efe1daa3dc7c1418105e1
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-01-31 11:48:03 -0300

    feat: adds handle, display_name, bio and born_at to User model and its validations

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# User
+class User < ApplicationRecord
+  validates :handle, presence: true, uniqueness: true, length: { in: 4..12 }, format: { with: /\A[a-zA-z0-9_]*\z/ }
+  validates :display_name, presence: true, length: { maximum: 30 }
+  validates :bio, length: { maximum: 300 }, allow_blank: true
+  validates :born_at, presence: true
+  validate :user_must_be_above_min_age
+
+  private
+
+  MIN_AGE = 13
+
+  def user_must_be_above_min_age
+    errors.add(:born_at, 'must be over 13 years old') unless born_at.present? && born_at <= MIN_AGE.years.ago.to_date
+  end
+end
