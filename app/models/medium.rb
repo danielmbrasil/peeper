3d610b701a7b2187803917ef453ec56c328de6ff
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-02-07 09:10:52 -0300

    refactor: removes before_validation callback and uses validate instead

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -9,13 +9,13 @@ class Medium < ApplicationRecord
   validates :medium_type, presence: true, numericality: { only_integer: true }
   validates :url, presence: true, allow_blank: false, format: { with: URI::DEFAULT_PARSER.make_regexp }
 
-  before_validation :validate_media_limit
+  validate :media_limit
 
   private
 
   MEDIA_LIMIT = 4
 
-  def validate_media_limit
+  def media_limit
     return unless status.present?
 
     errors.add(:media, 'a status cannot contain over four media') if status.media.size > MEDIA_LIMIT
